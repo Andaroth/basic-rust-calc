c4631e42ee37d0ca223e800e38fffa4e1d5846f7
Anda <ax.fiolle@gmail.com>
2024-10-11 01:10:29 +0200

    Improve general experience

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::io;
 
 fn print_menu() {
-    println!("AB calc, choisissez");
+    println!("Choisissez une opération:");
     println!("[1] Addition");
     println!("[2] Soustraction");
     println!("[3] Division");
     println!("[4] Reste");
+    println!("[5] Quitter")
 }
 
 fn main() {
+    println!("=================\n==   AB calc   ==\n=================\n");
     let input = &mut String::new();
     let input_a = &mut String::new();
     let input_b = &mut String::new();
@@ -17,9 +19,10 @@ fn main() {
         input.clear();
         io::stdin().read_line(input).expect("Failed to read line");
         let select: i32 = input.trim().parse().expect("Entrez un nombre!");
-        if select < 1 || select > 4 {
+        if select < 1 || select > 5 {
             println!("Choisissez une option valide (de 1 à 4)");
         } else {
+            if select == 5 { break; }
             // prompt a
             input_a.clear();
             println!("Choisissez un premier terme:");
@@ -32,12 +35,25 @@ fn main() {
             let b: f64 = input_b.trim().parse().expect("Entrez un nombre!");
 
             let mut res: f64 = 0.0;
-            if select == 1 { res = a + b; }
-            if select == 2 { res = a - b; }
-            if select == 3 { res = a / b; }
-            if select == 4 { res = a % b; }
+            let mut symbol: &str = "";
+            if select == 1 {
+                res = a + b;
+                symbol = "+";
+            }
+            if select == 2 {
+                res = a - b;
+                symbol = "-";
+            }
+            if select == 3 {
+                res = a / b;
+                symbol = "/";
+            }
+            if select == 4 {
+                res = a % b;
+                symbol = "%";
+            }
 
-            println!("Résultat: {}", res);
+            println!("\n\n\n{} {} {} = {} \n\n\n", a, symbol, b, res);
         }
     }
 }

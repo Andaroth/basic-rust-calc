e9c4687ba0ee60a827c923d18890022ef737ddb4
Anda <ax.fiolle@gmail.com>
2024-10-11 21:04:58 +0200

    alotta comments

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,9 @@ use std::fmt::Debug;
 
 static ERROR_MESSAGE: &str = "Une erreur s'est produite";
 static INVALID_FLOAT: &str = "Ce n'est pas un nombre valide";
-fn print_error(text: &str) {
-    println!("\n\x1b[31m{}\x1b[0m\n", text);
-}
+fn print_error(text: &str) { println!("\n\x1b[31m{}\x1b[0m\n", text); }
 
+// implemented math functions
 fn add(a: f64, b: f64) -> f64 { a + b }
 fn subtract(a: f64, b: f64) -> f64 { a - b }
 fn multiply(a: f64, b: f64) -> f64 { a * b }
@@ -16,6 +15,7 @@ fn divide(a: f64, b: f64) -> f64 { a / b }
 fn remainder(a: f64, b: f64) -> f64 { a % b }
 
 static OPERATIONS: [(&'static str, fn(f64, f64) -> f64, char); 5] = [
+    // Catalog operations (name, function, symbol)
     ("Addition", add, '+'),
     ("Soustraction", subtract, '-'),
     ("Multiplication", multiply, '*'),
@@ -24,59 +24,60 @@ static OPERATIONS: [(&'static str, fn(f64, f64) -> f64, char); 5] = [
 ];
 
 fn print_menu() {
+    // shows all option + quit
     for (i,m) in OPERATIONS.iter().enumerate() { println!("[{}] {}", i + 1, &m.0); }
     println!("[{}] Quitter\n", OPERATIONS.len() + 1);
 }
 
-fn is_str_number<T: FromStr>(text: &str) -> bool { return text.trim().parse::<T>().is_ok(); }
-fn str_to_number<T: FromStr>(text: &str) -> T where <T as FromStr>::Err: Debug { return text.trim().parse::<T>().expect(&ERROR_MESSAGE); }
+fn is_str_number<T: FromStr>(text: &str) -> bool { return text.trim().parse::<T>().is_ok(); } // verify if parsable string to number
+fn str_to_number<T: FromStr>(text: &str) -> T where <T as FromStr>::Err: Debug { return text.trim().parse::<T>().expect(&ERROR_MESSAGE); } // conversion
 
 fn read_value(print: &str) -> String {
     print!("{}: ", print);
-    io::stdout().flush().unwrap();
+    io::stdout().flush().unwrap(); // prompt on 1 line
     let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).ok().expect(ERROR_MESSAGE);
+    io::stdin().read_line(&mut buffer).ok().expect(ERROR_MESSAGE); // prompt to ref
     return buffer;
 }
 
 fn main() {
-    println!("\x1b[34m=================\n==   AB calc   ==\n=================\n\x1b[0m");
+    println!("\x1b[32m=================\n=    AB calc    =\n=================\x1b[0m\n"); // styled header
     loop {
         print_menu();
         let input = read_value("Choisissez une opération");
-        if !is_str_number::<i32>(&input) {
+        if !is_str_number::<i32>(&input) { // error handling
             print_error("Saisie invalide, veuillez recommencer");
-            continue;
+            continue; // restart loop
         }
-        let select: usize = str_to_number::<usize>(&input);
-        if select < 1 || select > OPERATIONS.len() + 1 {
-            // coercion & concat of a warning message
+        let select: usize = str_to_number::<usize>(&input); // usize to select operations by index
+        if select < 1 || select > OPERATIONS.len() + 1 { // not existing option
+            // coercion & concat of a warning message:
             let mut warn = "Choisissez une option valide (de 1 à ".to_owned(); // static left side of str
             warn.push_str(&(OPERATIONS.len() + 1).to_string()); // calculated, to str
             warn.push_str(")"); // static right side of str
             print_error(&warn);
-        } else {
-            if select == OPERATIONS.len() + 1 { break; }
-            println!("\n\x1b[34m<<{}>>\x1b[0m\n", OPERATIONS[select - 1].0);
-            // prompt a
-            let input = read_value("Choisissez le premier terme");
-            if !is_str_number::<f64>(&input) {
+        } else { // constrained to existing options
+            if select == OPERATIONS.len() + 1 { break; } // "quit" option, kill loop (end program)
+            println!("\n\x1b[34m<<{}>>\x1b[0m\n", OPERATIONS[select - 1].0); // name of selected operation
+            
+            let input = read_value("Choisissez le premier terme"); // prompt a
+            if !is_str_number::<f64>(&input) { // error handling
                 print_error(INVALID_FLOAT);
-                continue;
+                continue; // restart loop
             }
-            let a: f64 = str_to_number::<f64>(&input);
-            // prompt b
-            let input = read_value("Choisissez le second terme");
-            if !is_str_number::<f64>(&input) {
+            let a: f64 = str_to_number::<f64>(&input); // save prompt a to stack
+            
+            let input = read_value("Choisissez le second terme"); // prompt b
+            if !is_str_number::<f64>(&input) { // error handling
                 print_error(INVALID_FLOAT);
-                continue;
+                continue; // restart loop
             }
-            let b: f64 = str_to_number::<f64>(&input);
+            let b: f64 = str_to_number::<f64>(&input); // save prompt b to stack
 
-            let (_name, operation, symbol) = OPERATIONS[select - 1];
-            let ans: f64 = operation(a,b);
+            let (name, operation, symbol) = OPERATIONS[select - 1]; // extract selected operation details
+            let ans: f64 = operation(a,b); // execute operation function
 
-            println!("\n\x1b[31m{} {} {} = {}\x1b[0m \n", a, symbol, b, ans);
+            println!("\n\x1b[32m{}: {} {} {} = \x1b[33m{}\x1b[0m \n", name, a, symbol, b, ans); // show answer
         }
     }
 }

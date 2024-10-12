4a223ecc8ea8e33d62f6065bce72ffe2c4986152
Axel <ax.fiolle@gmail.com>
2024-10-12 04:14:06 +0200

    give details about math operations

--- src/math.rs
+++ src/math.rs
@@ -4,7 +4,13 @@ fn multiply(a: f64, b: f64) -> f64 { a * b }
 fn divide(a: f64, b: f64) -> f64 { a / b }
 fn remainder(a: f64, b: f64) -> f64 { a % b }
 
-pub static OPERATIONS: [(&'static str, fn(f64, f64) -> f64, char); 5] = [
+type Operation = (
+    &'static str, // name
+    fn(f64, f64) -> f64, // math operation
+    char // display symbol
+);
+
+pub static OPERATIONS: [Operation; 5] = [
     // Catalog operations (name, function, symbol)
     ("Addition", add, '+'),
     ("Soustraction", substract, '-'),
